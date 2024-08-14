2dd97d996fca1f9c90c5960e166c0562571a8a60
Carlos Toledo <carlombo@hotmail.com>
2024-08-14 17:03:49 -0400

    Se muestran flanes publicos en index y privados en welcome

--- web/views.py
+++ web/views.py
@@ -3,13 +3,9 @@ from web.models import Flan
 
 # Create your views here.
 def index(request):
-    flanes = Flan.objects.all()
-    flanes_privados = Flan.objects.filter(is_private=True)
     flanes_publicos = Flan.objects.filter(is_private=False)
     context = {
-        'flanes': flanes,
-        'flanes_privados': flanes_privados,
-        'flanes_publicos': flanes_publicos
+        'flanes': flanes_publicos
     }
     return render(request, 'index.html', context)
 
@@ -18,4 +14,8 @@ def about(request):
     return render(request, 'about.html')
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    flanes_privados = Flan.objects.filter(is_private=True)
+    context = {
+        'flanes': flanes_privados
+    }
+    return render(request, 'welcome.html', context)
